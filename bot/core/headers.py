a8ba7c7fb4881b423512d556249d950403500d31
controlpanel <resepadelili@gmail.com>
2024-05-17 13:07:39 -0500

    adakami cryptod

---
+++ bot/core/headers.py
@@ -0,0 +1,14 @@
+headers = {
+    'Accept': 'application/json',
+    'Accept-Language': 'ru-RU,ru;q=0.9',
+    'Content-Type': 'application/json',
+    'Origin': 'https://tg-app.memefi.club',
+    'Referer': 'https://tg-app.memefi.club/',
+    'Sec-Ch-Ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+    'Sec-Ch-Ua-Mobile': '?1',
+    'Sec-Ch-Ua-Platform': '"Android"',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'User-Agent': 'Telegram/9.4.1 (Android 6.0; Nexus 5 Build/MRA58N; rv:123.0)',
+}
